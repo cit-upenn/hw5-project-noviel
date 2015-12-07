a2eb932ed1cf09e6267ce095eeb4d8c871f83ff5
Tristan Tianle Yang <Tristan Tianle Yang>
2015-12-07 15:53:00 -0500

    search->list initialized

--- app/src/main/java/com/example/tristantianle/noviel2/SearchList.java
+++ app/src/main/java/com/example/tristantianle/noviel2/SearchList.java
@@ -1,9 +1,7 @@
 package com.example.tristantianle.noviel2;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 
 public class SearchList extends AppCompatActivity {
 
@@ -11,12 +9,5 @@ public class SearchList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-
     }
-
 }
