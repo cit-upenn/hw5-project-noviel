e0d6038c6e55842168f6f70ad7d8a39aa3bf514a
Tristan Tianle Yang <Tristan Tianle Yang>
2015-12-07 18:52:06 -0500

    search->list working with weather app

--- app/src/main/java/com/example/tristantianle/noviel2/SearchList.java
+++ app/src/main/java/com/example/tristantianle/noviel2/SearchList.java
@@ -1,7 +1,8 @@
 package com.example.tristantianle.noviel2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SearchList extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class SearchList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_list);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new SearchFragment())
+                    .commit();
+        }
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
     }
 }
